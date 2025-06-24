66c50d1e6dae70ac8eacbcc67e483cd072446607
MarcoLor01 <marcolorenzini23@gmail.com>
2025-06-24 14:25:24 +0200

    Resolved bug in DBSCAN

--- src/main/java/flink/utilities/Constants.java
+++ src/main/java/flink/utilities/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
     public static final int WINDOW_LAYERS_NUMBER = 3;
     public static final int INTERNAL_NEIGHBORS_DISTANCE = 2;
     public static final int OUTLIER_THRESHOLD = 6000;
-    public static final int DBSCAN_RADIUS = 20;
-    public static final int DBSCAN_MIN_PTS = 5;
+    public static final double DBSCAN_RADIUS = 20.0;
+    public static final int DBSCAN_MIN_PTS = 4;
 }
